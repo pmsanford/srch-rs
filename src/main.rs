078a89b2fb3332de29b114589485330eec936b1b
Paul Sanford <me@paulsanford.net>
2014-12-20 02:30:13 -0500

    Getopts stuff

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 extern crate miniparse;
 extern crate syntax;
+extern crate getopts;
 
+use getopts::{optflag, getopts, OptGroup};
 use std::os;
 use std::io::{File, FileMode, FileAccess};
 use std::io::fs::{PathExtensions, walk_dir};
@@ -12,16 +14,53 @@ use syntax::ptr::P;
 
 fn main() {
     let args: Vec<String> = os::args();
+    let program = args[0].clone();
+
+    let opts = &[
+        optflag("a", "", "Match all"),
+        optflag("x", "", "Top-level items"),
+        optflag("m", "", "Methods (fns in impls)"),
+        optflag("t", "", "Trait methods (fns in traits)"),
+        optflag("f", "", "Struct fields"),
+        ];
+
+    let opt_strs = ["a", "x", "m", "t", "f"];
+    let opt_strings: Vec<String> = 
+        opt_strs.iter().map(|&x| x.to_string()).collect();
+
+    let matches = match getopts(args.tail(), opts) {
+        Ok(m) => { m },
+        Err(f) => { panic!(f.to_string()); }
+    };
+
+    let term = if !matches.free.is_empty() {
+        matches.free[0].clone()
+    } else {
+        print_usage(program.as_slice(), opts);
+        return;
+    };
+
+    let match_all = 
+        matches.opt_present("a") || 
+        !matches.opts_present(opt_strings.as_slice());
+
+    let tl = matches.opt_present("x") || match_all;
+    let m = matches.opt_present("m") || match_all;
+    let t = matches.opt_present("t") || match_all;
+    let f = matches.opt_present("f") || match_all;
+
     let p = os::getcwd().unwrap();
-    let srch_str = args[1].as_slice();
+    let srch_str = term.as_slice();
     for path in walk_dir(&p).unwrap() {
         if path.is_file() && path.as_str().unwrap().ends_with(".rs") {
-            try_match(&p, &path, srch_str);
+            try_match(&p, &path, srch_str,
+                     tl, m, t, f);
         }
     }
 }
 
-fn try_match(cwd: &Path, path: &Path, srch: &str) {
+fn try_match(cwd: &Path, path: &Path, srch: &str,
+             tl: bool, m: bool, t: bool, f: bool) {
     let mut file = File::open_mode(path, 
                                    FileMode::Open, 
                                    FileAccess::Read).unwrap();
@@ -30,12 +69,18 @@ fn try_match(cwd: &Path, path: &Path, srch: &str) {
     let mpr = miniparse::parse_crate(source.clone(), path_str);
 
     for item_ptr in mpr.cr.module.items.iter() {
-        if item_ptr.ident.name.as_str() == srch {
+        if item_ptr.ident.name.as_str() == srch && tl {
             print_line(cwd, &mpr, item_ptr.span);
         }
-        match_impl_funcs(cwd, &mpr, item_ptr, srch);
-        match_trait_funcs(cwd, &mpr, item_ptr, srch);
-        match_struct_members(cwd, &mpr, item_ptr, srch);
+        if m {
+            match_impl_funcs(cwd, &mpr, item_ptr, srch);
+        }
+        if t {
+            match_trait_funcs(cwd, &mpr, item_ptr, srch);
+        }
+        if f {
+            match_struct_members(cwd, &mpr, item_ptr, srch);
+        }
     }
 }
 
@@ -98,4 +143,8 @@ fn print_line(cwd: &Path, mpr: &miniparse::Miniresult, spn: Span) {
              relpath.display(),
              mpr.get_line_from_span(spn) + 1, // 0-indexed
              mpr.get_line_text_from_span(spn));
+}
+
+fn print_usage(program: &str, _opts: &[OptGroup]) {
+    println!("Usage: {} search_term", program);
 }
