8145005020729a03bad10bb6e29a849e06a95007
Paul Sanford <me@paulsanford.net>
2014-12-19 22:35:08 -0500

    All paths below current dir

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,33 @@ extern crate miniparse;
 
 use std::os;
 use std::io::{File, FileMode, FileAccess};
+use std::io::fs::{PathExtensions, walk_dir};
 
 fn main() {
     let args: Vec<String> = os::args();
-    let p = Path::new(args[1].clone());
-    let mut file = File::open_mode(&p, 
+    let p = os::getcwd().unwrap();
+    let srch_str = args[1].as_slice();
+    for path in walk_dir(&p).unwrap() {
+        if path.is_file() && path.as_str().unwrap().ends_with(".rs") {
+            try_match(&p, &path, srch_str);
+        }
+    }
+}
+
+fn try_match(cwd: &Path, path: &Path, srch: &str) {
+    let mut file = File::open_mode(path, 
                                    FileMode::Open, 
                                    FileAccess::Read).unwrap();
     let source = file.read_to_string().unwrap();
-    let mpr = miniparse::parse_crate(source.clone(), args[1].clone());
+    let path_str = String::from_str(path.as_str().unwrap());
+    let mpr = miniparse::parse_crate(source.clone(), path_str);
 
     for item_ptr in mpr.cr.module.items.iter() {
-        if item_ptr.ident.name.as_str() == args[2].as_slice() {
+        if item_ptr.ident.name.as_str() == srch {
+            let pth = Path::new(mpr.file_map.name.clone());
+            let relpath = pth.path_relative_from(cwd).unwrap();
             println!("{}:{}: {}", 
-                     mpr.file_map.name,
+                     relpath.display(),
                      mpr.get_line_from_span(item_ptr.span) + 1, // 0-indexed
                      mpr.get_line_text_from_span(item_ptr.span));
         }
